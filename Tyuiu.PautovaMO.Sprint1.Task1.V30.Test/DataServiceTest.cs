91430a62e633dcad45995aec658962e7bb85dd30
PautovaMO <Pautova812007@mail.ru>
2025-09-27 23:28:14 +0500

    Task1

---
+++ Tyuiu.PautovaMO.Sprint1.Task1.V30.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PautovaMO.Sprint1.Task1.V30.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint1.Task1.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            var res= ds.Calculate(x);
+            Assert.AreEqual(2, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.PautovaMO.Sprint1.Task1.V30/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.PautovaMO.Sprint1.Task1.V30.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint1.Task1.V30
+{
+    class programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил : Паутова М.О. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема:  Организация ввода/вывода в консольных приложениях                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (2+x)/2 и печатает его на экране.        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            Console.WriteLine("Введите значение Х:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+
+            Console.ReadLine();
+        }
+    }
+}
