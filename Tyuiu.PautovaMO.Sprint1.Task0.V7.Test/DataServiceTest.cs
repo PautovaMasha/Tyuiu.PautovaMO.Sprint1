7d9ad8af1b421a3114798a4eff52db84fef6a345
PautovaMO <Pautova812007@mail.ru>
2025-09-27 15:19:35 +0500

    Add project files.

---
+++ Tyuiu.PautovaMO.Sprint1.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PautovaMO.Sprint1.Task0.V7.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint1.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(5, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.PautovaMO.Sprint1.Task0.V7/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.PautovaMO.Sprint1.Task0.V7.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint1.Task0.V7
+{
+    class programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил : Паутова М.О. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значение двух    *");
+            Console.WriteLine("* Одинаковых массивов по длине.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 18/6*2 - (4-3)                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
