d6548c23769419eecff1e47cbaf262ca629664af
PautovaMO <Pautova812007@mail.ru>
2025-09-28 22:28:02 +0500

    Task4

---
+++ Tyuiu.PautovaMO.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PautovaMO.Sprint1.Task4.V5.Lib;
+namespace Tyuiu.PautovaMO.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double x = 2;
+            double y = 4;
+            
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.25, res);
+
+        }
+    }
+}
---
+++ Tyuiu.PautovaMO.Sprint1.Task4.V5/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.PautovaMO.Sprint1.Task4.V5.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint1.Task4.V5
+{
+    class programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил : Паутова М.О. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема:  Class Math                                                       *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            Console.WriteLine(" Введите X: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            double y;
+            Console.WriteLine(" Введите Y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+       
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" 1 / | x + √y | = " + ds.Calculate(x, y));
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
