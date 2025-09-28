c8a5506d199e70e79b80ee52d13fa058551dab05
PautovaMO <Pautova812007@mail.ru>
2025-09-28 23:21:31 +0500

    Task5

---
+++ Tyuiu.PautovaMO.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PautovaMO.Sprint1.Task5.V3.Lib;
+namespace Tyuiu.PautovaMO.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int k = 130985;
+            var res = ds.Calculate(k);
+            int result = Convert.ToInt32(res);
+            int wait = 9;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.PautovaMO.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.PautovaMO.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint1.Task5.V3
+{
+    class programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил : Паутова М.О. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Присвоить целой переменной h третью от конца цифру                      *");
+            Console.WriteLine("* в записи положительного целого числа k                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int k;
+
+            Console.WriteLine("Введите значение K:");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                             *");
+            Console.WriteLine("**********************************************************************************************************");
+
+            Console.WriteLine("Переменная h = " + ds.Calculate(k));
+            Console.ReadLine();
+
+            int res = Convert.ToInt32(ds.Calculate(k));
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
